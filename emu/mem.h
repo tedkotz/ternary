b5ffca6a3dabf5f4bea400762bd8107cfbfc2a52
Theodore Kotz <ted@kotz.us>
2021-01-24 10:10:34 -0500

    Added a Makefile Create memory and cpu specific files for refactoring Moved RAM to more general memory file

---
+++ emu/mem.h
@@ -0,0 +1,57 @@
+// 3^11 Trites of RAM
+#define RAM_SIZE 177147
+#define BASE_OFFSET (RAM_SIZE/2)
+#define RAM_ADDR_MASK ((1<<(11*2))-1)
+
+typedef enum
+{
+    ADDR_ISR_INV_INSTR = -2,
+    ADDR_ISR_DIV_ZERO  = -3,
+    ADDR_UART_IN       = -4,
+	ADDR_UART_OUT      = -5,
+	ADDR_RTC           = -6,
+	/*BITMAP DISPLAY*/
+	/*DAC*/
+	/*ADC*/
+	/*TIMER_INT*/
+} MemMappedDevices;
+
+Trite ram[RAM_SIZE];
+
+Trite ReadRam(TriWord addr)
+{
+	int64_t offset=TriWord2int(addr & RAM_ADDR_MASK);
+	switch(offset)
+	{
+		case ADDR_UART_IN:
+			// return int2Triword(getchar());
+			return 0;
+			break;
+		case ADDR_UART_OUT:
+			return 0;
+			break;
+		case ADDR_RTC:
+			// return int2TriWord(time(null));
+			return 0;
+			break;
+		default:
+			return ram[BASE_OFFSET+offset];
+	}
+}
+
+void WriteRam(TriWord addr, Trite Val)
+{
+	int64_t offset=TriWord2int(addr & RAM_ADDR_MASK);
+	switch(offset)
+	{
+		case ADDR_UART_IN:
+		case ADDR_RTC:
+			break;
+		case ADDR_UART_OUT:
+			putchar(TriWord2int(val));
+			break;
+		default:
+			ram[BASE_OFFSET+offset]=val;
+	}
+
+}
